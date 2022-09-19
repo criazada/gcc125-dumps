4c5e8ae5096b7c4e24bff3d1c1dd2fee97e32574
iagocq <18238046+iagocq@users.noreply.github.com>
2022-09-19 14:01:14 -0300

    * -> dump

--- dump.sh
+++ dump.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+folder=dump
+
 download() {
     content="$(curl -Ls --max-time 10 "$1")"
     if [ -n "$content" ]; then
+        pushd "$2"
         echo "Downloading $1"
         wget --quiet --recursive --no-parent "$1"
+        popd
     fi
 }
 
+mkdir -p "$folder"
 for i in $(seq 1 255); do
-  download "http://192.168.1.$i" &
+  download "http://192.168.1.$i" "$folder" &
 done
 
 download "http://192.168.1.37/relatorios"
-
